5bb7a6a6ffffa100cb15423c126e184b17ccca32
Tim Wood <timwood@gwu.edu>
2016-02-06 17:29:49 -0500

    Explain how cores are used in ONVM start script

--- onvm/go.sh
+++ onvm/go.sh
@@ -8,6 +8,8 @@ then
         echo "$0 [cpu-list] [port-bitmask]"
         # this works well on our 2x6-core nodes
         echo "$0 0,1,2,6 3 --> cores 0, 1, 2 and 6 with ports 0 and 1"
+        echo "Cores will be used as follows in numerical order:"
+        echo "  RX thread, TX thread, ..., TX thread for last NF, Stats thread"
         exit 1
 fi
 
