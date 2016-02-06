4d85ca0c5e25411247c45d09be0e371adbca0732
Tim Wood <timwood@gwu.edu>
2016-02-06 17:19:48 -0500

    script for ONVM that takes core list and portmask

--- onvm/go.sh
+++ onvm/go.sh
@@ -1,5 +1,15 @@
-# run on cores 0,1,2 (first socket) and 6 (second socket)
-# this works well on our 2x6-core nodes
-sudo rm -rf /mnt/huge/*
-sudo ./onvm_mgr/onvm_mgr/x86_64-native-linuxapp-gcc/onvm_mgr -l 0,1,2,6 -n 3 --proc-type=primary  -- -p1
+#!/bin/bash
 
+cpu=$1
+ports=$2
+
+if [ -z $ports ]
+then
+        echo "$0 [cpu-list] [port-bitmask]"
+        # this works well on our 2x6-core nodes
+        echo "$0 0,1,2,6 3 --> cores 0, 1, and 9 with ports 0 and 1"
+        exit 1
+fi
+
+sudo rm -rf /mnt/huge/rtemap_*
+sudo ./onvm_mgr/onvm_mgr/x86_64-native-linuxapp-gcc/onvm_mgr -l $cpu -n 3 --proc-type=primary  -- -p${ports}
