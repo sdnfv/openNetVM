e4f17848576ee68474b87a02db339a87fde201fe
Grace Liu <guyue.liu@gmail.com>
2016-03-18 18:49:58 -0400

    app: test loop latency

--- examples/loop_latency/loop_latency.c
+++ examples/loop_latency/loop_latency.c
@@ -17,7 +17,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- * rewrite.c - an example using onvm. Rewrite the payload for each p package received
+ * loop_latency.c - create pkts and loop through NFs to test the layency.
+ * This example refers to dpdk rxtx_callback code.
  ********************************************************************/
 
 #include <unistd.h>
@@ -33,23 +34,37 @@
 
 #include <rte_common.h>
 #include <rte_mbuf.h>
-#include <rte_memcpy.h>
 #include <rte_ip.h>
-#include <rte_udp.h>
+#include <rte_mempool.h>
+#include <rte_cycles.h>
+#include <rte_memcpy.h>
 
 #include "onvm_nflib.h"
 #include "onvm_pkt_helper.h"
 
-#define NF_TAG "simple_forward"
+#define NF_TAG "latecny"
+
+#define NUM_PKTS 128
+#define PKTMBUF_POOL_NAME "MProc_pktmbuf_pool"
+
+#define FLAG ((uint32_t)123456789)
 
 /* Struct that contains information about this NF */
 struct onvm_nf_info *nf_info;
 
+struct stamp {
+	uint32_t flag;
+	uint64_t now;
+};
+
+static struct {
+	uint64_t total_cycles;
+	uint64_t total_pkts;
+} latency_numbers;
+
 /* number of package between each print */
-static uint32_t print_delay = 1000000;
-
-
-static uint32_t destination;
+static uint32_t print_delay = 10000000;
+static uint16_t destination;
 
 /*
  * Print a usage message
@@ -66,7 +81,7 @@ static int
 parse_app_args(int argc, char *argv[], const char *progname) {
         int c;
 
-        while ((c = getopt(argc, argv, "d:p:")) != -1) {
+        while ((c = getopt (argc, argv, "d:p:")) != -1) {
                 switch (c) {
                 case 'd':
                         destination = strtoul(optarg, NULL, 10);
@@ -101,60 +116,90 @@ parse_app_args(int argc, char *argv[], const char *progname) {
  */
 static void
 do_stats_display(struct rte_mbuf* pkt) {
+        static uint64_t last_cycles;
+        static uint64_t cur_pkts = 0;
+        static uint64_t last_pkts = 0;
         const char clr[] = { 27, '[', '2', 'J', '\0' };
         const char topLeft[] = { 27, '[', '1', ';', '1', 'H', '\0' };
-        static int pkt_process = 0;
-        struct ipv4_hdr* ip;
+        (void)pkt;
 
-        pkt_process += print_delay;
+        uint64_t cur_cycles = rte_get_tsc_cycles();
+        cur_pkts += print_delay;
 
         /* Clear screen and move to top left */
         printf("%s%s", clr, topLeft);
 
-        printf("PACKETS\n");
-        printf("-----\n");
-        printf("Port : %d\n", pkt->port);
-        printf("Size : %d\n", pkt->pkt_len);
-        printf("N°   : %d\n", pkt_process);
-        printf("\n\n");
+        printf("Total packets: %9"PRIu64" \n", cur_pkts);
+        printf("TX pkts per second: %9"PRIu64" \n", (cur_pkts - last_pkts)
+                * rte_get_timer_hz() / (cur_cycles - last_cycles));
+        printf("Packets per group: %d\n", NUM_PKTS);
 
-        ip = onvm_pkt_ipv4_hdr(pkt);
-        if (ip != NULL) {
-                onvm_pkt_print(pkt);
-        } else {
-                printf("No IP4 header found\n");
-        }
+        last_pkts = cur_pkts;
+        last_cycles = cur_cycles;
+
+        printf("\n\n");
+}
+
+static void
+add_timestamps(struct rte_mbuf *pkt)
+{
+	uint64_t now = rte_rdtsc();
+	char* data;
+
+	data = rte_pktmbuf_prepend(pkt, sizeof(struct stamp));
+	if (data != NULL) {
+		((struct stamp*)data)->flag = FLAG;
+		((struct stamp*)data)->now = now;
+	}
+}
+
+static void
+calc_latency(struct rte_mbuf *pkt)
+{
+	uint64_t now = rte_rdtsc();
+	struct stamp* data = rte_pktmbuf_mtod(pkt, struct stamp*);
+	
+	latency_numbers.total_cycles += now - data->now;
+	latency_numbers.total_pkts++;
+	rte_pktmbuf_adj(pkt, sizeof(struct stamp));
+}
+
+static void
+print_latency(void)
+{
+	printf("Latency = %"PRIu64" cycles\n",
+		latency_numbers.total_cycles / latency_numbers.total_pkts);
+	latency_numbers.total_cycles = latency_numbers.total_pkts = 0;
 }
 
 static int
 packet_handler(struct rte_mbuf* pkt, struct onvm_pkt_meta* meta) {
         static uint32_t counter = 0;
-	struct ipv4_hdr *ipv4_hdr = onvm_pkt_ipv4_hdr(pkt);
-	struct udp_hdr *udp_hdr = onvm_pkt_udp_hdr(pkt);
-	uint16_t ip_len, udp_len, data_len;
-	char *data;        
-	const char *newdata = "123\0";
+        struct stamp* data;
 	
-	if (++counter == print_delay) {
-                do_stats_display(pkt);
+	if (counter++ == print_delay) {
+	        do_stats_display(pkt);
+      		print_latency();	
                 counter = 0;
         }
-	
-	if (onvm_pkt_is_udp(pkt)) {
-		ip_len = rte_be_to_cpu_16(ipv4_hdr->total_length);
-		udp_len = rte_be_to_cpu_16(udp_hdr->dgram_len);
-		data_len = ip_len - udp_len;
-		if (data_len > strlen(newdata)) {
-			data = (char *)((char*)udp_hdr + sizeof(struct udp_hdr)); 
-			rte_memcpy(data, newdata, strlen(newdata));
-			//printf("udp payload %s\n", data);
+
+	if(pkt->port == 3) {
+		data = rte_pktmbuf_mtod(pkt, struct stamp*);
+		if (data != NULL && data->flag == FLAG) {
+			calc_latency(pkt);
+		}       
+        	else {
+			add_timestamps(pkt);
 		}
-	}
- 
-        meta->action = ONVM_NF_ACTION_TONF;
-        meta->destination = destination;
-        
-	return 0;
+	        /* one of our fake pkts to forward */
+                meta->destination = destination;
+		meta->action = ONVM_NF_ACTION_TONF;
+        }
+        else {
+                /* Drop real incoming packets */
+                meta->action = ONVM_NF_ACTION_DROP;
+        }
+        return 0;
 }
 
 
@@ -167,10 +212,31 @@ int main(int argc, char *argv[]) {
                 return -1;
         argc -= arg_offset;
         argv += arg_offset;
-	destination = nf_info->service_id + 1;
+
+        destination = nf_info->service_id;
 
         if (parse_app_args(argc, argv, progname) < 0)
                 rte_exit(EXIT_FAILURE, "Invalid command-line arguments\n");
+
+        struct rte_mempool *pktmbuf_pool;
+        struct rte_mbuf* pkts[NUM_PKTS];
+        int i;
+
+        pktmbuf_pool = rte_mempool_lookup(PKTMBUF_POOL_NAME);
+        if(pktmbuf_pool == NULL) {
+                rte_exit(EXIT_FAILURE, "Cannot find mbuf pool!\n");
+        }
+        printf("Creating %d packets to send to %d\n", NUM_PKTS, destination);
+        for (i=0; i < NUM_PKTS; i++) {
+                struct onvm_pkt_meta* pmeta;
+                pkts[i] = rte_pktmbuf_alloc(pktmbuf_pool);
+                pmeta = onvm_get_pkt_meta(pkts[i]);
+                pmeta->destination = destination;
+                pmeta->action = ONVM_NF_ACTION_TONF;
+		pkts[i]->port = 3;
+                pkts[i]->hash.rss = i;
+		onvm_nf_return_pkt(pkts[i]);
+        }
 
         onvm_nf_run(nf_info, &packet_handler);
         printf("If we reach here, program is ending");
