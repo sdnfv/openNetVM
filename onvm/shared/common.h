f0e60ef384a235aa0d3450ae57795b9210ee4a55
Parham Alvani <parham.alvani@gmail.com>
2016-06-11 10:07:10 +0430

    ONVM_PORT_BROADCAST macro was defined.

--- onvm/shared/common.h
+++ onvm/shared/common.h
@@ -54,6 +54,8 @@
 #define ONVM_NF_ACTION_TONF 2   // send to the NF specified in the argument field (assume it is on the same host)
 #define ONVM_NF_ACTION_OUT 3    // send the packet out the NIC port set in the argument field
 
+#define ONVM_PORT_BROADCAST     // broadcast port
+
 //extern uint8_t rss_symmetric_key[40];
 
 struct onvm_pkt_meta {
