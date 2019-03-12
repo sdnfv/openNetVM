d52c9e455996c6b489c5da15d71246eba5e47f9f
Mykola Yurchenko <urchennko@gmail.com>
2019-03-11 21:07:01 -0400

    Update onvm_common.h

--- onvm/onvm_nflib/onvm_common.h
+++ onvm/onvm_nflib/onvm_common.h
@@ -51,7 +51,7 @@
 #include "onvm_config_common.h"
 
 #define ONVM_MAX_CHAIN_LENGTH 4   // the maximum chain length
-#define MAX_NFS 128               // total number of NFs allowed
+#define MAX_NFS 32                // total number of NFs allowed
 #define MAX_SERVICES 32           // total number of unique services allowed
 #define MAX_NFS_PER_SERVICE 32    // max number of NFs per service.
 
