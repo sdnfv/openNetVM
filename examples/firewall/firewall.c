6a0bdff7d5b81d08ccfbcc8e4cbb2a2746fbf963
Dennis Afanasev <dennisafa@gwu.edu>
2019-03-14 00:21:15 -0400

    Update firewall.c

--- examples/firewall/firewall.c
+++ examples/firewall/firewall.c
@@ -263,7 +263,7 @@ struct onvm_fw_rule** setup_rules(int* total_rules, char* rules_file) {
             if (getcwd(dir, sizeof(dir)) > 0) {
                     char *par = dirname(dir);
                     char slash_tmp[strlen(rules_file)+1];
-                    char *file_slash = strcat(slash, rules_file);
+                    char *file_slash = strcat(slash_tmp, rules_file);
                     printf("%s", file_slash);
                     //strcat(par, set_dir);
                     //rules_json = onvm_config_parse_file(rules_set);
