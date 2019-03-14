34854443556cd4ee36962e665c021efd7825f967
Dennis Afanasev <dennisafa@gwu.edu>
2019-03-14 00:38:10 -0400

    Update firewall.c

--- examples/firewall/firewall.c
+++ examples/firewall/firewall.c
@@ -261,7 +261,7 @@ struct onvm_fw_rule** setup_rules(int* total_rules, char* rules_file) {
             char dir[PATH_MAX];
             if (getcwd(dir, sizeof(dir)) > 0) {
                     char *par = dirname(dir);
-                    char *slash_tmp = calloc(strlen(rules_file));
+                    char *slash_tmp = calloc(strlen(rules_file), sizeof(char));
                     slash_tmp[0] = '/';
                     char *file_slash = strcat(slash_tmp, rules_file);
                     printf("%s", file_slash);
