f75e4a98da12477c74184e01f5d983dd460e5939
dennisa <dennisa@node-0.mtcp.gwcloudlab-pg0.wisc.cloudlab.us>
2019-03-18 20:06:57 -0500

    Nuking custom go.sh script

--- examples/firewall/go.sh
+++ examples/firewall/go.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-#The go.sh script is a convinient way to run start_nf.sh without specifying NF_NAME
-
-NF_DIR=${PWD##*/}
-
-if [ ! -f ../start_nf.sh ]; then
-  echo "ERROR: The ./go.sh script can only be used from the NF folder"
-  echo "If running from other directory use examples/start_nf.sh"
-  exit 1
-fi
-
-cd .. && ./start_nf.sh $NF_DIR "$@"
+../go.sh
