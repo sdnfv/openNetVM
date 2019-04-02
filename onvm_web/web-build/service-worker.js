4a961d3135b9a20977b2aca867506f47ce7db1b6
Kevin Deems <kevin8deems@gmail.com>
2019-04-01 23:37:42 -0500

    Removed husky

--- onvm_web/web-build/service-worker.js
+++ onvm_web/web-build/service-worker.js
@@ -1,11 +1,1 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js"
-);
-importScripts("/precache-manifest.8e6120d97baacfcf5f917b7cc7b81aad.js");
-workbox.clientsClaim();
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
-});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");importScripts("/precache-manifest.f87c568b31b18191b564b7558daff62f.js");workbox.clientsClaim();self.__precacheManifest=[].concat(self.__precacheManifest||[]);workbox.precaching.suppressWarnings();workbox.precaching.precacheAndRoute(self.__precacheManifest,{});workbox.routing.registerNavigationRoute("/index.html",{blacklist:[/^\/_/,/\/[^\/]+\.[^\/]+$/]});
