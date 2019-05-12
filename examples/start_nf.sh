a5df37c3a26427fbca89c4efd54412e4f18915b6
Kevin Deems <kevin8deems@gmail.com>
2019-05-12 13:31:10 -0500

    Fixed string arguments for go script

--- examples/start_nf.sh
+++ examples/start_nf.sh
@@ -48,7 +48,11 @@ fi
 
 # Check if -- is present, if so parse dpdk/onvm specific args
 dash_dash_cnt=0
+arg_cnt=0
 for i in "$@" ; do
+  if [[ dash_dash_cnt -le 1 ]] ; then
+        arg_cnt=$((arg_cnt+1))
+  fi
   if [[ $i == "--" ]] ; then
     dash_dash_cnt=$((dash_dash_cnt+1))
   fi
@@ -58,7 +62,9 @@ done
 if [[ $dash_dash_cnt -ge 2 ]]; then
   DPDK_ARGS="$DPDK_BASE_ARGS $(echo " ""$@" | awk -F "--" '{print $1;}')"
   ONVM_ARGS="$(echo " ""$@" | awk -F "--" '{print $2;}')"
-  NF_ARGS="$(echo " ""$@" | awk -F "--" '{print $3;}')"
+
+  # Move to NF arguments
+  shift ${arg_cnt}
 elif [[ $dash_dash_cnt -eq 0 ]]; then
   # Dealing with required args shared by all NFs
   service=$1
@@ -66,7 +72,6 @@ elif [[ $dash_dash_cnt -eq 0 ]]; then
 
   DPDK_ARGS="-l $DEFAULT_CORE_ID $DPDK_BASE_ARGS"
   ONVM_ARGS="-r $service"
-  NF_ARGS="$@"
 elif [[ $dash_dash_cnt -eq 1 ]]; then
   # Don't allow only one `--`
   echo "This script expects 0 or at least 2 '--' argument separators"
@@ -74,4 +79,4 @@ elif [[ $dash_dash_cnt -eq 1 ]]; then
   exit 1
 fi
 
-exec sudo $BINARY $DPDK_ARGS -- $ONVM_ARGS -- $NF_ARGS 
+exec sudo $BINARY $DPDK_ARGS -- $ONVM_ARGS -- "$@"
