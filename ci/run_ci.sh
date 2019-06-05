3a97ae46a889b82e5117dbfff3003b58990ee193
Kevin Deems <kevin8deems@gmail.com>
2019-06-05 18:48:25 -0400

    Add flask to install

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -6,8 +6,6 @@ SCRIPT_LOC=$(pwd)
 
 print_header "Installing dependencies"
 
-set +e
-
 sudo apt update
 sudo apt install -y build-essential libssl-dev libffi-dev
 sudo apt install -y python3
@@ -22,6 +20,9 @@ check_exit_code "ERROR: Pip not installed"
 sudo -H pip3 install virtualenv --upgrade
 check_exit_code "ERROR: virtualenv had trouble upgrading"
 
+sudo -H pip3 install flask --upgrade 
+check_exit_code "ERROR: Flask not installed or failed to install"
+
 sudo -H pip3 install --pre github3.py
 check_exit_code "ERROR: GitHub3.py not installed or failed to install"
 
@@ -30,3 +31,7 @@ check_exit_code "ERROR: Paramiko not installed or failed to install"
 
 sudo -H pip3 install pexpect
 check_exit_code "ERROR: Pexpect not installed or failed to install"
+
+print_header "Done Installing, running CI"
+# run the web server with the input arguments
+python3 webhook-receiver.py $1 $2 $3 $4
