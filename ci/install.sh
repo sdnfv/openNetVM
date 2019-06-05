74676e5b592318c01e1d20687691a21b72871231
Kevin Deems <kevin8deems@gmail.com>
2019-06-04 21:33:02 -0400

    Save performance in ci

---
+++ ci/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# install all dependencies only once
+set -e
+. helper-functions.sh
+SCRIPT_LOC=$(pwd)
+
+print_header "Installing dependencies"
+
+set +e
+
+sudo apt update
+sudo apt install -y build-essential libssl-dev libffi-dev
+sudo apt install -y python3
+sudo apt install -y python3-pip
+
+python3 -V
+check_exit_code "ERROR: Python not installed"
+
+pip3 -V
+check_exit_code "ERROR: Pip not installed"
+
+sudo -H pip3 install virtualenv --upgrade
+check_exit_code "ERROR: virtualenv had trouble upgrading"
+
+sudo -H pip3 install --pre github3.py
+check_exit_code "ERROR: GitHub3.py not installed or failed to install"
+
+sudo -H pip3 install paramiko
+check_exit_code "ERROR: Paramiko not installed or failed to install"
+
+sudo -H pip3 install pexpect
+check_exit_code "ERROR: Pexpect not installed or failed to install"
