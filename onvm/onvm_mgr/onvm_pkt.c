6bf1c9f9319bfd362f44e84ed101de0717ef52a2
Kevin Deems <kevin8deems@gmail.com>
2019-06-28 18:17:07 -0400

    Add Macro for Running Flow Table Lookup (#147)
    Running a flow table lookup on every packet is expensive and as the
    functionality isn't always used we have decided to create a macro to
    disable it. The functionality will still be enabled by default, but if
    it is unused disabling it should boost performance. 
    
    Commit log:
    
    * Macros for running flow table lookup
    
    * Updated comment about disabling flow table lookup

--- onvm/onvm_mgr/onvm_pkt.c
+++ onvm/onvm_mgr/onvm_pkt.c
@@ -57,9 +57,11 @@ void
 onvm_pkt_process_rx_batch(struct queue_mgr *rx_mgr, struct rte_mbuf *pkts[], uint16_t rx_count) {
         uint16_t i;
         struct onvm_pkt_meta *meta;
+#ifdef FLOW_LOOKUP
         struct onvm_flow_entry *flow_entry;
         struct onvm_service_chain *sc;
         int ret;
+#endif
 
         if (rx_mgr == NULL || pkts == NULL)
                 return;
@@ -68,15 +70,19 @@ onvm_pkt_process_rx_batch(struct queue_mgr *rx_mgr, struct rte_mbuf *pkts[], uin
                 meta = (struct onvm_pkt_meta *)&(((struct rte_mbuf *)pkts[i])->udata64);
                 meta->src = 0;
                 meta->chain_index = 0;
+#ifdef FLOW_LOOKUP
                 ret = onvm_flow_dir_get_pkt(pkts[i], &flow_entry);
                 if (ret >= 0) {
                         sc = flow_entry->sc;
                         meta->action = onvm_sc_next_action(sc, pkts[i]);
                         meta->destination = onvm_sc_next_destination(sc, pkts[i]);
                 } else {
+#endif
                         meta->action = onvm_sc_next_action(default_chain, pkts[i]);
                         meta->destination = onvm_sc_next_destination(default_chain, pkts[i]);
+#ifdef FLOW_LOOKUP
                 }
+#endif
                 /* PERF: this might hurt performance since it will cause cache
                  * invalidations. Ideally the data modified by the NF manager
                  * would be a different line than that modified/read by NFs.
