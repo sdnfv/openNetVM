7577ce07e27e43ebb24978910a1058ea96e136f0
Kevin Deems <kevin8deems@gmail.com>
2019-12-24 16:34:32 +0000

    Commit everything from dependencies

--- onvm_web/web-build/service-worker.js
+++ onvm_web/web-build/service-worker.js
@@ -1,1 +1,11 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");importScripts("/precache-manifest.67a6849a35f8ccd1355871a20fd23ef6.js");workbox.clientsClaim();self.__precacheManifest=[].concat(self.__precacheManifest||[]);workbox.precaching.suppressWarnings();workbox.precaching.precacheAndRoute(self.__precacheManifest,{});workbox.routing.registerNavigationRoute("/index.html",{blacklist:[/^\/_/,/\/[^/]+\.[^/]+$/]});
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js"
+);
+importScripts("/precache-manifest.6ea0b81975f1b4420e86704c622f1872.js");
+workbox.clientsClaim();
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerNavigationRoute("/index.html", {
+  blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/]
+});
