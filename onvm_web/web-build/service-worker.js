dc1b1f7310a59b48249487a1c637c52f9a95164d
Kevin Deems <kevin8deems@gmail.com>
2019-12-24 16:39:59 +0000

    Compress all files

--- onvm_web/web-build/service-worker.js
+++ onvm_web/web-build/service-worker.js
@@ -1,11 +1,1 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js"
-);
-importScripts("/precache-manifest.6ea0b81975f1b4420e86704c622f1872.js");
-workbox.clientsClaim();
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/]
-});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");importScripts("/precache-manifest.b0875427682e94ec6386885a6954b74b.js");workbox.clientsClaim();self.__precacheManifest=[].concat(self.__precacheManifest||[]);workbox.precaching.suppressWarnings();workbox.precaching.precacheAndRoute(self.__precacheManifest,{});workbox.routing.registerNavigationRoute("/index.html",{blacklist:[/^\/_/,/\/[^/]+\.[^/]+$/]});
