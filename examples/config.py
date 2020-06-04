d00d5a8c6d1ccaf66246cb4dc74234ee24fe1a42
Catherine Meadows <cmeadows@nimbnode21.nimbus.seas.gwu.edu>
2020-06-04 06:55:19 +0000

    working script and example json config

--- examples/config.py
+++ examples/config.py
@@ -1,24 +1,21 @@
 import sys
 import json
 import os
+import shlex
 import time
-from signal import signal, SIGINT
-from multiprocessing import Process
+from signal import signal, SIGINT, SIGTERM
 from subprocess import Popen, PIPE
+import subprocess
 
-# cleanup
+# cleanup on user shutdown
 def handler(signal_received, frame):
     print("Exiting...")
     for p in procs_list:
-        p.kill()
+        try: 
+            p.kill()
+        except OSError:
+            pass
     sys.exit(0)
-
-# check that all processes started 
-def check_process():
-    while True:
-        for j in jobs:
-            if not j.is_alive():
-                sys.exit(1)
 
 # get path to config file 
 def get_config():
@@ -42,27 +39,36 @@ def remove_prefix(text, prefix):
         return text[len(prefix):]
     return text
 
-# handle shutdown of all NFs
-def on_failure():
-    print("Exiting...")
-    for p in procs_list:
-        p.kill()
+# handle shutdown of all NFs on error
+def on_failure(err):
+    print(err)
+    for proc in procs_list:
+        try:
+            proc.kill()
+        except OSError:
+            pass
+    for nf in nf_list:
+        try: 
+            os.system("sudo pkill " + nf)
+        except OSError:
+            pass
+    print("Error occurred. Exiting...")
     sys.exit(1)
 
 procs_list = []
 nf_list = []
 cmds_list = []
 if __name__ == '__main__':
-    # handle cleanup
+    # user shutdown
     signal(SIGINT, handler)
 
     # get path to config file
     path = get_config()
 
-    # open config file
+    # open file
     with open(path) as f:
         data = json.load(f)
-    # parse 
+    # parse config
     for k, v in data.items():
         for item in v:
             nf_list.append(k)
@@ -70,19 +76,22 @@ if __name__ == '__main__':
             param = remove_prefix(param, "u'parameters': u'")
             cmds_list.append("./go.sh " + param)
     
+    # start NFs 
     cwd = os.getcwd()
     for cmd, nf in zip(cmds_list, nf_list):
         os.chdir(nf)
-        procs_list = [Popen(cmd, stdout=PIPE, shell=True)]
+        p = subprocess.Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+        procs_list.append(p)
         os.chdir(cwd)
-
-    # check that NFs started without errors
+    
+    # check NFs are running
     while 1:
         for p in procs_list:
             ret_code = p.poll()
-            if ret_code is not None: 
-                on_failure()
+            if ret_code is not None:
+                out, err = p.communicate()
+                on_failure(out + err)
                 break
-            else: 
+            if ret_code is not None:
                 time.sleep(.1)
                 continue
