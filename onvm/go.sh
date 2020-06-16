83b19b789d4474074dccd8c08f1db4ad9e86d23b
EthanBaron14 <ethanbaron@gwu.edu>
2020-06-16 23:49:07 +0000

    Moved port check to prevent future merge conflict

--- onvm/go.sh
+++ onvm/go.sh
@@ -62,21 +62,6 @@ then
     usage
 fi
 
-# Convert the port mask to binary
-# Using bc where obase=2 indicates the output is base 2 and ibase=16 indicates the output is base 16
-ports_bin=$(echo "obase=2; ibase=16; $ports" | bc)
-# Splice out the 0's from the binary numbers. The result is only 1's. Example: 1011001 -> 1111
-ports_bin="${ports_bin//0/}"
-# The number of ports is the length of the string of 1's. Using above example: 1111 -> 4
-count_ports="${#ports_bin}"
-
-ports_detected=$("$RTE_SDK"/usertools/dpdk-devbind.py --status-dev net | sed '/Network devices using kernel driver/q' | grep -c "drv")
-if [[ $ports_detected -lt $count_ports ]]
-then
-    echo "Error: Invalid port mask. Insufficient NICs bound."
-    exit 1
-fi
-
 while getopts "a:r:d:s:t:l:p:z:cv" opt; do
     case $opt in
         a) virt_addr="--base-virtaddr=$OPTARG";;
@@ -93,6 +78,21 @@ while getopts "a:r:d:s:t:l:p:z:cv" opt; do
             ;;
     esac
 done
+
+# Convert the port mask to binary
+# Using bc where obase=2 indicates the output is base 2 and ibase=16 indicates the output is base 16
+ports_bin=$(echo "obase=2; ibase=16; $ports" | bc)
+# Splice out the 0's from the binary numbers. The result is only 1's. Example: 1011001 -> 1111
+ports_bin="${ports_bin//0/}"
+# The number of ports is the length of the string of 1's. Using above example: 1111 -> 4
+count_ports="${#ports_bin}"
+
+ports_detected=$("$RTE_SDK"/usertools/dpdk-devbind.py --status-dev net | sed '/Network devices using kernel driver/q' | grep -c "drv")
+if [[ $ports_detected -lt $count_ports ]]
+then
+    echo "Error: Invalid port mask. Insufficient NICs bound."
+    exit 1
+fi
 
 verbosity_level="-v $verbosity"
 
