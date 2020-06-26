a3bea189134583962ca5afd82a9260f35c17f6db
root <root@node1.mingyuma-qv74156.gwcloudlab-pg0.wisc.cloudlab.us>
2020-06-26 10:49:21 -0500

    Build react app

--- onvm_web/web-build/service-worker.js
+++ onvm_web/web-build/service-worker.js
@@ -1,1 +1,11 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");importScripts("/precache-manifest.9e30993647e06e9411d5539e546b5a8f.js");workbox.clientsClaim();self.__precacheManifest=[].concat(self.__precacheManifest||[]);workbox.precaching.suppressWarnings();workbox.precaching.precacheAndRoute(self.__precacheManifest,{});workbox.routing.registerNavigationRoute("/index.html",{blacklist:[/^\/_/,/\/[^/]+\.[^/]+$/]});
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js"
+);
+importScripts("/precache-manifest.28a724f0f2006c06cc0f95b05ea9c977.js");
+workbox.clientsClaim();
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerNavigationRoute("/index.html", {
+  blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/]
+});
