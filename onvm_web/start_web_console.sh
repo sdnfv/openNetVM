6d11086e9984200861345dd3340c8cb049742a15
mingyuma <mingyuma@node1.mingyuma-qv74915.gwcloudlab-pg0.wisc.cloudlab.us>
2020-07-01 07:05:25 -0500

    Fix runtime issue

--- onvm_web/start_web_console.sh
+++ onvm_web/start_web_console.sh
@@ -61,8 +61,8 @@ while getopts "p:" opt; do
 done
 
 # Get localhost IP address
-ifconfig | grep inet | grep -v inet6 | grep -v 127 | cut -d '' -f2 | awk '{print $2}'
-export HOSTIP=$!
+# ifconfig | grep inet | grep -v inet6 | grep -v 127 | cut -d '' -f2 | awk '{print $2}'
+# export HOSTIP=$!
 
 # Start ONVM web stats console at http://localhost:<port num>
 echo -n "Starting openNetVM Web Stats Console at http://localhost:"
