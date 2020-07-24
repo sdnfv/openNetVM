70b8352c759d2a54b353283cb929ddb45d7ecca9
WilliamMaa <williamma1998@outlook.com>
2020-07-24 13:50:41 -0400

    Update LaunchNFChain.react.js

--- onvm_web/react-app/src/pages/LaunchNFChain.react.js
+++ onvm_web/react-app/src/pages/LaunchNFChain.react.js
@@ -110,66 +110,67 @@ class LaunchNFChainPage extends Component {
 
   render() {
     return (
-        <div
-          style={{
-            marginLeft: "50px"
-          }}
-        >
+      <div
+        style={{
+          marginLeft: "50px"
+        }}
+      >
+        <br />
+        <h1>Network Function Chain Deployment</h1>
+        <h4>Upload a JSON Configuration File to Launch a Chain of NFs</h4>
+        <p>
+          Ensure your ONVM manager is running before uploading and launching
+          your chain of NFs. Navigate to the various dashboards on ONVM web to
+          observe NF behavior.
           <br />
-          <h1>Network Function Chain Deployment</h1>
-          <h4>Upload a JSON Configuration File to Launch a Chain of NFs</h4>
-          <p>
-            Ensure your ONVM manager is running before uploading and launching
-            your chain of NFs. Navigate to the various dashboards on ONVM web to
-            observe NF behavior.
-            <br />
-            Output from each NF will be written to the directory specified in your
-            file or to a default timestamped directory.
-          </p>
-          <p>
-            Follow the{" "}
-            <a href="https://github.com/catherinemeadows/openNetVM/blob/configScript/docs/NF_Dev.md">
-              documentation
-            </a>{" "}
-            in the ONVM repository to learn more about proper formatting for your
-            config file. See an{" "}
-            <a href="https://github.com/catherinemeadows/openNetVM/blob/configScript/examples/example_chain.json">
-              example
-            </a>{" "}
-            config file.
-          </p>
-          <div>
-            <input type="file" onChange={this.onFileChange} />
-            <button onClick={this.onFileUpload}>Upload</button>
-            <br />
-            <br />
-            <button
-              onClick={this.onLaunchChain}
-              style={{
-                backgroundColor: "#48cf7c",
-                borderRadius: "4px",
-                border: "none",
-                padding: "14px 28px"
-              }}
-            >
-              Launch NF Chain
-            </button>
-            <button
-              onClick={this.OnStopHandler}
-              style={{
-                margin: "5px",
-                backgroundColor: "#db4d5b",
-                borderRadius: "4px",
-                border: "none",
-                padding: "14px 28px"
-              }}
-            >
-              Terminate
-            </button>
-          </div>
+          Output from each NF will be written to the directory specified in your
+          file or to a default timestamped directory.
+        </p>
+        <p>
+          Follow the{" "}
+          <a href="https://github.com/catherinemeadows/openNetVM/blob/configScript/docs/NF_Dev.md">
+            documentation
+          </a>{" "}
+          in the ONVM repository to learn more about proper formatting for your
+          config file. See an{" "}
+          <a href="https://github.com/catherinemeadows/openNetVM/blob/configScript/examples/example_chain.json">
+            example
+          </a>{" "}
+          config file.
+        </p>
+        <div>
+          <input type="file" onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>Upload</button>
+          <br />
+          <br />
+          <button
+            onClick={this.onLaunchChain}
+            style={{
+              backgroundColor: "#48cf7c",
+              borderRadius: "4px",
+              border: "none",
+              padding: "14px 28px"
+            }}
+          >
+            Launch NF Chain
+          </button>
+          <button
+            type="submit"
+            onSubmit={this.submitHandler}
+            style={{
+              margin: "5px",
+              backgroundColor: "#db4d5b",
+              borderRadius: "4px",
+              border: "none",
+              padding: "14px 28px"
+            }}
+          >
+            Terminate
+          </button>
         </div>
+        {this.fileData()}
+      </div>
     );
   }
 }
-
 export default LaunchNFChainPage;
