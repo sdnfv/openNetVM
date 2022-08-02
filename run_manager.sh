de747d01f3dd0286b28142ffee1f3396f08ba83b
jiyan <mecitjiyan@gmail.com>
2022-08-02 13:56:18 +0300

    delete run_manager.sh

--- run_manager.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-cd /local/onvm/openNetVM/scripts
-source ./setup_cloudlab.sh
-cd /local/onvm/openNetVM
-cd onvm && make && cd ..
-cd examples && make && cd ..
-./onvm/go.sh -k 3 -n 0xFF -s stdout
